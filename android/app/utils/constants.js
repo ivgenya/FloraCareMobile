c6b70b95248fee3f61e1202ba1e13da116791782
Evgeniya Malinina <evgeniyamalinina0707@gmail.com>
2024-12-12 16:35:28 +0300

    Пути к серверу и api

--- android/app/utils/constants.js
+++ android/app/utils/constants.js
@@ -1,7 +1,3 @@
-//export const BASE_URL = 'http://192.168.31.30:8080/api';
-//export const BASE_UPLOADS_URL = 'http://192.168.31.30:8080';
-//export const API_URL = 'http://192.168.31.30:5555/plants';
-
-export const BASE_URL = 'http://192.168.112.160:8080/api';
-export const BASE_UPLOADS_URL = 'http://192.168.112.160:8080';
-export const API_URL = 'http://192.168.112.160:5555/plants';
+export const BASE_URL = 'http://floracare.ru/api';
+export const BASE_UPLOADS_URL = 'http://floracare.ru/';
+export const API_URL = 'http://plants-api.ru/plants';
